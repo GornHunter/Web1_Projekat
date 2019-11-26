0d82cdf4d7ac1a28969f634981d73ed08b4555f6
GornHunter <39407395+GornHunter@users.noreply.github.com>
2019-11-26 19:46:42 +0100

    dodata registracija

--- Web1_Projekat/Web1_Projekat/Controllers/ApartmanController.cs
+++ Web1_Projekat/Web1_Projekat/Controllers/ApartmanController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web1_Projekat.Models;
 
 namespace Web1_Projekat.Controllers
 {
@@ -17,6 +19,24 @@ namespace Web1_Projekat.Controllers
         public ActionResult Registracija()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registracija(Korisnik k)
+        {
+            UpisiKorisnikaTxt(k);
+
+            return RedirectToAction("Index", "Apartman");
+        }
+
+        public void UpisiKorisnikaTxt(Korisnik k)
+        {
+            string text = $"{k.KorisnickoIme}-{k.Lozinka}-{k.Ime}-{k.Prezime}-{k.Pol}-{k.Uloga}";
+
+            using (StreamWriter sw = new StreamWriter("../gosti.txt", true))
+            {
+                sw.WriteLine(text);
+            }
         }
     }
 }
